b00ec0c0046f3d8c492b006e1ac2cf4e68ce3991
Daniel Nolan <DanNolan99@gmail.com>
2020-06-02 00:58:03 -0400

    adds exchange functionality

--- client/index.js
+++ client/index.js
@@ -2,7 +2,33 @@ import "./index.scss";
 
 const server = "http://localhost:3042";
 
-document.getElementById("exchange-address").addEventListener('input', (event) => {
-  fetch(server)
-    .then(console.log)
+document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
+  if(value === "") {
+    document.getElementById("balance").innerHTML = 0;
+    return;
+  }
+
+  fetch(`${server}/balance/${value}`).then((response) => {
+    return response.json();
+  }).then(({ balance }) => {
+    document.getElementById("balance").innerHTML = balance;
+  });
+});
+
+document.getElementById("transfer-amount").addEventListener('click', () => {
+  const sender = document.getElementById("exchange-address").value;
+  const amount = document.getElementById("send-amount").value;
+  const recipient = document.getElementById("recipient").value;
+
+  const body = JSON.stringify({
+    sender, amount, recipient
+  });
+
+  const request = new Request(`${server}/send`, { method: 'POST', body });
+
+  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
+    return response.json();
+  }).then(({ balance }) => {
+    document.getElementById("balance").innerHTML = balance;
+  });
 });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3042;
+
+// localhost can have cross origin errors
+// depending on the browser you use!
+app.use(cors());
+app.use(express.json());
 
 const balances = {
   "1": 100,
@@ -8,8 +14,17 @@ const balances = {
   "3": 75,
 }
 
-app.get('/balance', (req, res) => {
-  res.send('Hello World!');
+app.get('/balance/:address', (req, res) => {
+  const {address} = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post('/send', (req, res) => {
+  const {sender, recipient, amount} = req.body;
+  balances[sender] -= amount;
+  balances[recipient] = (balances[recipient] || 0) + +amount;
+  res.send({ balance: balances[sender] });
 });
 
 app.listen(port, () => {
